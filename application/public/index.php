5c9fbca1d9a49577295ac7b07d379db26cfa95d0
Yugandhar Suthari <yugandhar.ad@gmail.com>
2018-02-06 05:56:59 -0500

    learning docker intial

---
+++ application/public/index.php
@@ -0,0 +1,18 @@
+<pre><?php
+echo "Hostname 'redis' can be found at: " . gethostbyname('redis')."\n";
+
+$hostname='mysql';
+$username='app-user';
+$password='app-pass';
+$dbname='my-app';
+
+try {
+    $dbh = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+    echo "Connected to the database at hostname 'mysql': " . gethostbyname('mysql') . "\n";
+} catch(Exception $e) {
+    echo $e->getMessage();
+}
+
+?></pre>
